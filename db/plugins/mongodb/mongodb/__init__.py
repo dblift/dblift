a7e9c5da6e3f220cda76ebe05503e2a3ae69ea8c
cmodiano <cmodiano@gmail.com>
2026-08-11 19:53:34 -0400

    feat(mongodb): add query executor

--- db/plugins/mongodb/mongodb/__init__.py
+++ db/plugins/mongodb/mongodb/__init__.py
@@ -1,5 +1,6 @@
 """MongoDB provider components."""
 
 from db.plugins.mongodb.mongodb.connection_manager import MongoDbConnectionManager
+from db.plugins.mongodb.mongodb.query_executor import MongoDbQueryExecutor
 
-__all__ = ["MongoDbConnectionManager"]
+__all__ = ["MongoDbConnectionManager", "MongoDbQueryExecutor"]
